3931c6fb2b34680f301b00c1a285a221652b9f41
Jamila <j.r.cba@hotmail.com>
2023-12-13 20:39:39 +0300

    solved partly

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function fillButton(index, text) {
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
-     // The code here will be exectued if you press on OK button that will pop on the window 
+  if (confirm(`Horraaay, ${winner} wins!`)) {
+    // The code here will be exectued if you press on OK button that will pop on the window
   }
 }
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+// fillButton(1, "X");
+// fillButton(9, "O");
 
 /**
  *
@@ -24,13 +24,36 @@ fillButton(9, "O");
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+let cells = ["", "", "", "", "", "", "", "", ""];
+let player = "O";
 function clickButton(index) {
   console.log(`Button number ${index} is clicked`);
-  // Your main code here.
+  if (player == "X" && document.getElementById(index).innerHTML == "") {
+    player = "O";
+    fillButton(index, player);
+    cells[index - 1] = "O";
+  } else if (player == "O" && document.getElementById(index).innerHTML == "") {
+    player = "X";
+    fillButton(index, player);
+    cells[index - 1] = "X";
+  }
 }
 
 /**
  * (Optional) It's always a good idea to make a function for every single purpose.
  */
-// function checkWinner
+
+function checkWinner() {
+  if (
+    (cells[0] == "O" && cells[1] == "O" && cells[2] == "O") ||
+    (cells[0] == "X" && cells[1] == "X" && cells[2] == "X")
+  ) {
+    winningAlert(player);
+  } else if (
+    (cells[0] == "O" && cells[4] == "O" && cells[8] == "O") ||
+    (cells[0] == "X" && cells[4] == "X" && cells[8] == "X")
+  ) {
+    winningAlert(player);
+  }
+}
 // function restartGame
